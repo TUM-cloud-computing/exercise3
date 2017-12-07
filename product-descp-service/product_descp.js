c0c43eb11377fc92eb977c651add304038c610ae
Anil <anil@protonmail.ch>
2017-12-07 23:13:14 +0100

    Initial commit

---
+++ product-descp-service/product_descp.js
@@ -0,0 +1,12 @@
+module.exports = function (options) {
+    //Import the mock data json file
+    const mockData = require('./MOCK_DATA.json');
+
+    this.add('role:product')
+
+    //To DO: Add the patterns and their corresponding functions
+
+
+    //To DO: add the pattern functions and describe the logic inside the function
+
+}
